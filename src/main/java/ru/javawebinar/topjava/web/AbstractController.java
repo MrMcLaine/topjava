337ad88e4808730427d29c86e41007e443de218c
MrMcLaine <Lypovskyi@gmail.com>
2022-11-22 04:01:20 +0200

    Add JspMealController

--- src/main/java/ru/javawebinar/topjava/web/AbstractController.java
+++ src/main/java/ru/javawebinar/topjava/web/AbstractController.java
@@ -1,17 +1,26 @@
-package ru.javawebinar.topjava.web.meal;
+package ru.javawebinar.topjava.web;
 
-import org.springframework.stereotype.Controller;
-import ru.javawebinar.topjava.web.AbstractController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
 
-@Controller
-public class MealRestController extends AbstractController {
- /*   private static final Logger log = LoggerFactory.getLogger(MealRestController.class);
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
 
-    private final MealService service;
+import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
 
-    public MealRestController(MealService service) {
-        this.service = service;
-    }
+public abstract class AbstractController {
+    private static final Logger log = LoggerFactory.getLogger(AbstractController.class);
+
+    @Autowired
+    private MealService service;
 
     public Meal get(int id) {
         int userId = SecurityUtil.authUserId();
@@ -45,12 +54,12 @@ public class MealRestController extends AbstractController {
         service.update(meal, userId);
     }
 
-    *//**
+    /**
      * <ol>Filter separately
      * <li>by date</li>
      * <li>by time for every date</li>
      * </ol>
-     *//*
+     */
     public List<MealTo> getBetween(@Nullable LocalDate startDate, @Nullable LocalTime startTime,
                                    @Nullable LocalDate endDate, @Nullable LocalTime endTime) {
         int userId = SecurityUtil.authUserId();
@@ -58,5 +67,5 @@ public class MealRestController extends AbstractController {
 
         List<Meal> mealsDateFiltered = service.getBetweenInclusive(startDate, endDate, userId);
         return MealsUtil.getFilteredTos(mealsDateFiltered, SecurityUtil.authUserCaloriesPerDay(), startTime, endTime);
-    }*/
+    }
 }
